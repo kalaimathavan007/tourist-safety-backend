d069cb8c671b47a3b16cfdff92828d21080d74ad
RANGER-one007 <kalaimathavan143@gmail.com>
2026-07-12 13:28:32 +0530

    Fixed 404 error

--- server.js
+++ server.js
@@ -19,6 +19,10 @@ const io = socketIo(server, {
 app.use(cors());
 app.use(express.json());
 
+// 👇 
+app.get('/', (req, res) => {
+    res.send("Tourist Safety Backend is running successfully!");
+});
 // Routes
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/alerts', require('./routes/alerts'));
